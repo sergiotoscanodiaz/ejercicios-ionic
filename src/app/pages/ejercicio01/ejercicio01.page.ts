98fca39e328a0a1930cc6c83f8a0abc56eb0f7f8
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2020-11-09 08:43:18 +0100

    Ejercicio01 actualizado

--- src/app/pages/ejercicio01/ejercicio01.page.ts
+++ src/app/pages/ejercicio01/ejercicio01.page.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ejercicio01Page implements OnInit {
 
+  minValue = 0;
+  maxValue = 0;
+  randomNumber = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  generateNumber() {
+    this.randomNumber = +this.minValue + Math.floor(Math.random() * (this.maxValue - this.minValue + 1));
   }
 
 }
